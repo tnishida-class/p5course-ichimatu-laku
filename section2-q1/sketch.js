e0040c913b3f5b98f30579a9902d67d4c0ca1934
ichimatu-laku <2011069h@stu.kobe-u.ac.jp>
2021-10-19 11:59:49 +0900

    today's mission

--- section2-q1/sketch.js
+++ section2-q1/sketch.js
@@ -1,6 +1,14 @@
 // 小手調べ
 function setup(){
-  for(let i = 0; i < 10; i++){
-    // BLANK[1]
+
+  for(let i = 0; i < 11; i++){
+    noFill();
+    stroke(0,0,255);
+    ellipse(50, 50, i*10);
+    if(i>5){
+      stroke(255,0,0);
+      ellipse(50, 50, i*10);
+    }
+
   }
 }
