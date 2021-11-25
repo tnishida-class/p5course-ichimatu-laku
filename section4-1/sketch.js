1e163f7f167d6e756f9ce42b6c66c30c0620052e
ichimatu-laku <2011069h@stu.kobe-u.ac.jp>
2021-11-25 17:39:31 +0900

    Update.4-1

--- section4-1/sketch.js
+++ section4-1/sketch.js
@@ -1,6 +1,6 @@
 // テキスト「配列」～「配列を使った描画」までを収録
 function setup(){
-  createCanvas(400, 400);
+  createCanvas(500, 500);
   background(240);
 
   // 配列をランダムに初期化する
@@ -18,33 +18,53 @@ function setup(){
   console.log(sum);
 
   // ここから平均・最大・最小を求めます
-  let average, largest, smallest;
+  let average ,largest , smallest;
+  average = sum / scores.length;
   // BLANK[1]　平均値（ヒント average = 合計 / 配列の長さ）
 
   largest = 0;
   for(let i = 0; i < scores.length; i++){
+    if(largest<scores[i]){
+      largest = scores[i];
+    }
+
     // BLANK[2]　ヒント：今までの最大値 largest と scores[i] を比較する
   }
 
   smallest = 100;
   for(let i = 0; i < scores.length; i++){
+    if(smallest>scores[i]){
+      smallest=scores[i];
+    }
     // BLANK[3]　ヒント：最小値とだいたい同じ
   }
 
   // ここから棒グラフを描いていきます。まずは背景に横線をn本引く
   const n = 10;
   for(let i = 0; i < n; i++){ line(0, height * i / n, width, height * i / n); }
-  
+
   noStroke();
 
   for(let i = 0; i < scores.length; i++){
     const dx = width / scores.length;
     const h = height * scores[i] / 100;
+    if(scores[i] == smallest){
+      fill(0,0,255);
+    }
+    else if(scores[i]==largest){
+      fill(255,0,0);
+    }
     // BLANK[4] ヒント: 条件分岐を使って色を変更します
+    else{
+      fill(128);
+    }
     rect(i * dx + 2, height - h, dx - 4, h);
-    fill(0);
     text(scores[i].toPrecision(3), i * dx, height - h);
-  }
 
-  // BLANK[5] 平均点の線を引きます
+  }
+  stroke(0,255,0);
+  line(0,height - height * average / 100, width, height - height * average / 100);
+  fill(0);
+  text(average.toPrecision(3), 1, height - height * average / 100);
+// BLANK[5] 平均点の線を引きます
 }
